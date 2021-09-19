c90154a0f94920514426f7f70511b1bc82ea7e68
Bruno <bruno.rp45@gmail.com>
2021-09-18 21:34:52 -0300

    Exercício prático 2

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-    $traco = ' - ';
+    $traco = ' - ';  
+    
 
-    for($n=0; $n<10; $n++) {
-        for($n1=0; $n1<5; $n1++) {
-            echo $traco;
-        }
-        echo "<br/>";
-    };
+        for($n1=0; $n1<20; $n1++) {
+            
+            for($n2=0; $n2 <= $n1; $n2++) {
+                echo $traco;
+
+            }
+            echo "<br/>";
+        }   
+
+
 ?>
