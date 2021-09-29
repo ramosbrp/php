b976f2810e470ad3bb0fe821483026031e98389e
Bruno <bruno.rp45@gmail.com>
2021-09-28 23:32:31 -0300

    Introdução à HTTP Request

--- index.php
+++ index.php
@@ -3,7 +3,28 @@ require('./templates/header.php');
 
 
 
+
+
 ?>
 
 <a href="./paginas/home.php">HOME</a>
-<a href="./paginas/sobre.php">SOBRE</a>
+<a href="./paginas/sobre.php">SOBRE</a> <br> <br>
+
+<form action="recebedor.php" method="POST" >
+    <label for="">
+        Nome:
+        <br>
+        <input type="text" name="nome" >
+    </label>
+
+    <br><br>
+
+    <label for="">
+        Senha:
+        <br>
+        <input type="password" name="idade" >
+    </label>
+    <br>
+    <br>
+    <input type="submit" value="Enviar">
+</form>
